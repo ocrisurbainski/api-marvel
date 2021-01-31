2967dab9dd0be0e895e4a4a5aa98ea5bdcc7c9dc
Cristian Urbainski <cristian.urbainski@ciss.com.br>
2021-01-31 15:41:45 -0300

    Adicionado estrutura básica do projeto

---
+++ src/main/java/br/com/urbainski/api/marvel/ApiMarvelApplication.java
@@ -0,0 +1,13 @@
+package br.com.urbainski.api.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMarvelApplication.class, args);
+	}
+
+}
